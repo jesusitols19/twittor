4aae4dc85adf8cc0325ff12a45573db12050a6b8
Jesus Llanos Salirrosas <Jesus@gmail.com>
2024-08-10 15:13:39 -0500

    Apariencia en IOS

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE = "static-v2";
-const DYNAMIC_CACHE = "dynamic-v1";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 
@@ -45,15 +45,23 @@ self.addEventListener("install",e =>{
 
 self.addEventListener("activate", e=>{
 
-    const respuesta = caches.keys().then(keys => {
-        keys.forEach(key =>{
-            if(key !== STATIC_CACHE && key.includes("static")){
-               return caches.delete(key);
+    const respuesta = caches.keys().then( keys => {
+ 
+        keys.forEach( key => {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
             }
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
         });
+ 
     });
-
-    e.waitUntil(respuesta);
+ 
+    e.waitUntil( respuesta );
 });
 
 
